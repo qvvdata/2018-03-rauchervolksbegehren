d78469319f4efff648b9e75e54b75b344a4dd635
geraldgartner <Gerald>
2018-03-08 23:39:17 +0100

    update and new data

--- scripts/01-preprocessing.R
+++ scripts/01-preprocessing.R
@@ -1,3 +1,5 @@
+#rm(list=ls())
+
 numerize <- function(data,vars){
   data = as.data.frame(data)
   variables <- colnames(data)
@@ -17,6 +19,30 @@ unterstuetzungen <- read_excel("input/unterstuetzungen.xlsx",
                                 "numeric", "numeric", "numeric", 
                                "numeric"))%>%
   numerize(vars = c("name"))
+
+unterstuetzungen_8_3 <- read_excel("input/statistik_61.xlsx")%>%
+  mutate(gkz = gsub('G', '', GKZ), 
+         number = nchar(gkz))%>%
+  subset(number ==5)%>%
+  select(-GKZ)%>%
+  mutate(type1 = substr(gkz, 4, 5), 
+         type2= substr(gkz,2,5), 
+         type3 = substr(gkz,4,5)) %>%
+  numerize(vars = c("Name", "klasse", "type2", "typ3")) %>%
+  mutate(klasse =  if_else(Name == "Österreich", "at",
+                           if_else(gkz == 90001, "bl",
+         if_else(type2 == "0000" & Name !="Österreich", "bl",
+                 if_else(type2!="0000" & type1 == 0, "bez","gem"))))) 
+
+
+
+#check der datenintegrität
+unterstuetzungen_8_3_check <- unterstuetzungen_8_3%>%
+  group_by(klasse)%>%
+  summarise(sumwb = sum(Wahlberechtigte), 
+            sumunt = sum(Unterstützungen))
+
+#Bezirke aufgrund von Status Wiens als Bezirk und Gemeinde nicht übereinstimmend
 
 nrw2017 <- read_csv("input/nrw2017.csv")%>%
   numerize(vars = c("name"))%>%
