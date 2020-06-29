573ed42698ce993511efcf5ae380803604688727
fin <fin+github@xbhd.org>
2020-06-30 00:43:53 +0200

    add klimavolksbegehren karte

--- interaktiv/karte/src/data.js
+++ interaktiv/karte/src/data.js
@@ -57,6 +57,33 @@ maps['orfbegehren'] = {
 };
 
 
+maps['klimavolksbegehren'] = {
+  title: 'Wo die Zustimmung zum Klimavolksbegehren am höchsten ist',
+  csv: 'analysis_klimavolksbegehren.csv',
+  source: '-',
+  value: 'Anteil',
+  scale: 'quantile',
+  colorschemes: [['#000'],
+            ['#dacbe5','#b599cb','#9069b1','#693a97','#3f007d']
+            //['#dacbe5','#3f007d']
+  ],
+  detail: 'Stand: 30. Juni 2020',
+
+  tooltip: function(d, p, pctfmt, numfmt) {
+    if(d) {
+      return `<strong>${d.Name}</strong>: <br/>`
+        +`Unterschriften: ${numfmt(d['Unterschriften'])}<br />`
+        +`Wahlberechtigte: ${numfmt(d['Wahlberechtigte'])}<br />`
+        +`Anteil: <strong>${pctfmt(d['Anteil'])} %</strong><br />`
+        +`Steigerung in der Eintragungswoche: ${pctfmt(d['pct_diff'])}&nbsp;Prozentpunkte`
+
+    } else {
+      return `<strong>${p.name}, ${p.GKZ}</strong>: Keine Daten vorhanden`;
+    }
+  }
+};
+
+
 Object.keys(maps).map((x) => {maps[x].map=x});
 
 export { maps };
