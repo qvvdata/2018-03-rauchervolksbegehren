91f84d103862a84d164bc2a5e5a3dc95f6330872
Stefania Braunisch <stefanie.braunisch@qvv.at>
2020-06-29 14:54:58 +0200

    add klimavolksbegehren

---
+++ manifest.R
@@ -0,0 +1,11 @@
+
+library(tidyverse) # ggplot2, dplyr, tidyr, readr, purrr, tibble
+library(magrittr) # pipes
+library(stringr) # string manipulation
+library(readxl) # excel
+library(scales) # scales for ggplot2
+library(jsonlite) # json
+library(forcats) # easier factor handling,
+library(lintr) # code linting
+library(xlsx) #Excel
+library(googlesheets) # googlesheets (replace with googlesheets4 asap)
---
+++ scripts/preprocessing_klimavolksbegehren.R
@@ -0,0 +1,58 @@
+#rm(list=ls())
+library(tidyverse)
+library(readxl)
+
+numerize <- function(data,vars){
+  data = as.data.frame(data)
+  variables <- colnames(data)
+  variables <- variables[! variables %in% vars]
+  for(i in variables){
+    data[,i]<- as.numeric(data[,i])
+    data[,i][is.na(data[,i])] <- 0
+  }
+  return(data)
+}
+
+#source("scripts/BorderMan.R")
+
+#####klimavolksbegehren
+klimavolksbegehren <- read_excel("input/klimavolksbegehren_2020-06-26.xlsx")%>%
+  mutate(gkz = gsub('G', '', GKZ), 
+         number = nchar(gkz))%>%
+  subset(number ==5)%>%
+  select(-GKZ)%>%
+  mutate(type1 = substr(gkz, 4, 5), 
+         type2= substr(gkz,2,5), 
+         type3 = substr(gkz,4,5)) %>%
+  numerize(vars = c("Name", "klasse", "type2", "typ3")) %>%
+  mutate(klasse =  if_else(Name == "Österreich", "at",
+                           if_else(gkz == 90001, "bl",
+                                   if_else(type2 == "0000" & Name !="Österreich", "bl",
+                                           if_else(type2!="0000" & type1 == 0, "bez","gem"))))) %>% 
+  rename(GKZ=gkz) %>% 
+  mutate(GKZ=as.character(GKZ))
+
+
+#check der datenintegrität
+#Bezirke aufgrund von Status Wiens als Bezirk und Gemeinde nicht übereinstimmend
+
+klimavolksbegehren_check <- klimavolksbegehren%>%
+  group_by(klasse)%>%
+  summarise(sumwb = sum(Wahlberechtigte), 
+            sumunt = sum(Unterstützungen),
+            sumsum = sum(Summe))
+
+
+nrw2019 <- read_xlsx("input/nrw2019.xlsx")#%>%
+#numerize(vars = c("name"))
+#nrw2019 <- remove_teilungen(borderman(nrw2019))
+nrw2019$GKZ <- sub('.', '', nrw2019$GKZ)
+
+urbanrural <- read_excel("input/urbanrural_2019.xlsx", sheet = "DEGURBA 2019")%>%
+  numerize(vars = c("name", "urbantyp", "urbanländlich")) %>% 
+  mutate(GKZ=as.character(GKZ))
+
+
+contextdata <- nrw2019 %>% left_join(urbanrural, by=c("GKZ"))
+
+
